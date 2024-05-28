c1996850c3d59ccaa9da009d3771bcc4bf7ef001
Pablo Koll Ideapad <pablokollm@gmail.com>
2024-05-28 19:43:04 +0200

    update files names exports

--- export-cvs.sh
+++ export-cvs.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+branch_name=$(git rev-parse --abbrev-ref HEAD)
+echo "$branch_name"
+file_name=''
+if echo "$branch_name" | grep -q "english"; then
+    file_name='pablo koll cv.pdf'
+    echo $file_name
+else
+    file_name='cv pablo koll.pdf'
+    echo $file_name
+fi
+
 source './.env'
 
 cp './cv.pdf' $EXPORT_ROUTE  # Reemplaza las rutas por las adecuadas
+
+mv "$EXPORT_ROUTE/cv.pdf" "$EXPORT_ROUTE/$file_name"
