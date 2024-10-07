3cfdabf0e3156eae302ed437309955ebbcbc8d40
ideapad-w11-wsl <pablokollm@gmail.com>
2024-10-07 22:59:12 +0200

    updated experience cv

--- export-cv.sh
+++ export-cv.sh
@@ -1,38 +1,7 @@
 #!/bin/sh
 set -e
 
-# Verificar si xelatex está instalado
-if ! command -v xelatex &> /dev/null; then
-    echo "xelatex no está instalado. Intentando instalar..."
-
-    # Detectar el sistema operativo y usar el comando de instalación adecuado
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        if [ -x "$(command -v apt-get)" ]; then
-            echo "Installing texlive-xetex in Linux (apt-get)..."
-            sudo apt-get update
-            sudo apt-get install -y texlive-xetex
-        elif [ -x "$(command -v dnf)" ]; then
-            echo "Installing texlive-xetex in Linux (dnf)..."
-            sudo dnf install -y texlive-xetex
-        else
-            echo "No se pudo detectar el manejador de paquetes adecuado en Linux."
-            exit 1
-        fi
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        echo "Instalando MacTeX en macOS..."
-        # Aquí puedes poner el comando de instalación para macOS
-    elif [[ "$OSTYPE" == "msys"* ]]; then
-        echo "Instalando Tex en MSYS..."
-        pacman -S texlive-bin
-    else
-        echo "Sistema operativo no soportado para la instalación automática de xelatex."
-        exit 1
-    fi
-else
-    echo "xelatex ya está instalado."
-fi
-
-xelatex './cv.tex'
+npm run xelatex
 
 git add .
 # git commit -m 'updated cv file' --no-verify
@@ -51,5 +20,6 @@ fi
 source './.env'
 
 cp './cv.pdf' $EXPORT_ROUTE  # Reemplaza las rutas por las adecuadas
+
 
 mv "$EXPORT_ROUTE/cv.pdf" "$EXPORT_ROUTE/$file_name"
